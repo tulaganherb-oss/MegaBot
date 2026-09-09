50b538b9c8d6e91a107676a171717e9242993301
tulaganherb-oss <tulaganherb@gmail.com>
2026-09-10 00:10:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
-# Конфигурация супер-бота
 import os
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "8852428203:AAEsnP6EZHUhA1wH8GijIQRBoOttVcXVvqo")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AQ.Ab8RN6LshrFRuBpp77_HDzDZPJxUlLGFhqc331fb9qFaQ7SBmg")
-)
-AI_MODEL = "openai/gpt-4o-mini"
